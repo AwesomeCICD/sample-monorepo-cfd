66541742f1e8c2ae4076e64ee7eae62e224915ed
Sam Olukotun <sam.olukotun@circleci.com>
2022-07-26 16:41:15 -0700

    Add a test

--- frontend/tests/unit/componentCartItemCard.spec.ts
+++ frontend/tests/unit/componentCartItemCard.spec.ts
@@ -15,5 +15,18 @@ describe('CartItemCard.vue', () => {
         })
         expect(wrapper.text()).toContain('4.99')
     })
+    it('renders the menuItem name', () => {
+        const menuItem = {
+            id: 1,
+            name: "French Fries",
+            description: "Fried potato rectangles",
+            price: "4.99",
+            imageId: '1'
+        }
+        const wrapper = shallowMount(CartItemCard, {
+            props: {menuItem}
+        })
+        expect(wrapper.text()).toContain('French Fries')
+    })
 
 })
